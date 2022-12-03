526ca189573a7795f9485abf1f86c6b248084a77
Tibor Santa <maquirag@gmail.com>
2022-12-03 22:50:23 +0100

    day 1

--- src/aoc2022/core.clj
+++ src/aoc2022/core.clj
@@ -1,7 +1,44 @@
 (ns aoc2022.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main []
+  (println "Solving Advent of Code 2022"))
+
+(defn input
+  "Read a file from resource folder"
+  [file]
+  (slurp (io/resource file)))
+
+(defn d01-add-strings
+  "Add numbers in a list of strings"
+  [strings]
+  (reduce #(+ %1 (Integer/parseInt %2)) 0 strings))
+
+(defn d01-bags
+  "Parse the data and summarize each bag"
+  [data]
+  (let [groups (str/split data #"\n\n")
+        elves (map #(str/split % #"\n") groups)]
+    (map d01-add-strings elves)))
+
+(defn solve-d01-1
+  "Day 1 Task 1"
+  [data]
+  (->> data
+       (d01-bags)
+       (apply max)))
+
+(defn solve-d01-2
+  "Day 1 Task 2"
+  [data]
+  (->> data
+       (d01-bags)
+       (sort)
+       (reverse)
+       (take 3)
+       (reduce +)))
+
+(solve-d01-1 (input "dec01.txt"))
+(solve-d01-2 (input "dec01.txt"))
--- test/aoc2022/core_test.clj
+++ test/aoc2022/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [aoc2022.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest dec01-sample-test
+  (testing "Day 1 Task 1 sample"
+    (is (= 24000 (solve-d01-1 (input "dec01sample.txt")))))
+  (testing "Day 1 Task 2 sample"
+    (is (= 45000 (solve-d01-2 (input "dec01sample.txt"))))))
